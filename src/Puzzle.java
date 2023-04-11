bbd892bff0b8b991a35d62643556da40817bbe74
Albert <albert.austiniv@gmail.com>
2023-04-11 19:22:03 -0400

    Prepared the puzzle class and text file - Albert Austin IV

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,2 +1,18 @@
-public class Puzzle {
+import java.io.Serializable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Puzzle implements Serializable {
+
+    private Scanner infile;
+    private String puzzleID;
+    private String puzzleRoomID;
+    private String problem;
+    private String solution;
+    private String hint;
+    private String attempts;
+    private String correctOutcome;
+    private String failOutcome;
 }
